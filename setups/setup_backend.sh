5747240cb5d3714d010ea0908b162124de1fdd41
robwsc <robert.s.w.carroll@gmail.com>
2023-02-12 17:45:04 -0500

    add setup_backend.sh

---
+++ setups/setup_backend.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+version="1.0.0"
+name="setup_backend.sh"
+
+# check if python is installed
+if ! command -v python &>/dev/null; then
+  echo "Python could not be found"
+  exit
+fi
+
+# Prompt the user for the name of the project
+read -p "Enter the name of the Django project (hit enter for current directory): " project_name
+
+# check if project name is empty
+if [ -z "$project_name" ]; then
+  # set project name current folder name if empty
+  project_name=$(basename "$PWD")
+fi
+
+# Install packages using pip
+python_packages="djangorestframework django-cors-headers drf-yasg whitenoise"
+pip install $python_packages
+
+# check to see if file exists
+if [ -f "${project_name}"/settings.py ]; then
+  echo "Settings File exists"
+else
+  # prompt user to create a new project
+  read -p -r "File does not exist. Would you like to create a new project? (y/n): " create_project
+  if [ "$create_project" == "y" ]; then
+    # create a new project
+    django-admin startproject "${project_name}"
+  else
+    # exit the script
+    exit 1
+  fi
+fi
+
+# modify Django settings file to add necessary additions
+echo "Modifying Django settings file..."
+echo -e "\n\n# Added by $name $version (@robswc)" >>"${project_name}"/settings.py
+echo "INSTALLED_APPS += ['drf_yasg', 'corsheaders']" >>"${project_name}"/settings.py
+echo "MIDDLEWARE += ['corsheaders.middleware.CorsMiddleware', 'django.middleware.common.CommonMiddleware']" >>"${project_name}"/settings.py
+echo "MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']" >>"${project_name}"/settings.py
+echo 'STATIC_ROOT = BASE_DIR / "staticfiles"' >>"${project_name}"/settings.py
+echo "CORS_ALLOWED_ORIGINS = ['http://localhost:5000']" >>"${project_name}"/settings.py
+echo "Finished..."
